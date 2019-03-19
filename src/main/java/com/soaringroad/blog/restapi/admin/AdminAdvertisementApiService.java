84a13b9503b22ac5416c6a6094d5d15f333d97db
wangzhenhui1992 <605951224@qq.com>
2019-03-19 10:29:19 +0900

    拼写修正

--- src/main/java/com/soaringroad/blog/restapi/admin/AdminAdvertisementApiService.java
+++ src/main/java/com/soaringroad/blog/restapi/admin/AdminAdvertisementApiService.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/admin/advertisment")
+@RequestMapping("/api/admin/advertisement")
 public class AdminAdvertisementApiService extends AbstractSrBlogApiService<Advertisement, Long>{
 
     @Override
--- src/main/java/com/soaringroad/blog/restapi/visit/AdvertisementApiService.java
+++ src/main/java/com/soaringroad/blog/restapi/visit/AdvertisementApiService.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/advertisment")
+@RequestMapping("/api/advertisement")
 public class AdvertisementApiService extends AbstractSrBlogApiService<Advertisement, Long> {
 
   @Override
