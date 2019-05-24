c260b48e687bd0e6a9e7207472a89fd077b41e4a
wangzhenhui1992 <605951224@qq.com>
2019-05-24 12:43:02 +0900

    修正Entity

--- src/main/java/com/soaringroad/blog/repository/h2/AwsQuestionRepository.java
+++ src/main/java/com/soaringroad/blog/repository/h2/AwsQuestionRepository.java
@@ -1,10 +1,10 @@
 package com.soaringroad.blog.repository.h2;
 
-import com.soaringroad.blog.entity.common.Advertisement;
+import com.soaringroad.blog.entity.common.AwsQuestion;
 import com.soaringroad.blog.repository.SrBlogH2Repository;
 import org.springframework.stereotype.Component;
 
 @Component
-public interface AwsQuestionRepository extends SrBlogH2Repository<Advertisement, Long> {
+public interface AwsQuestionRepository extends SrBlogH2Repository<AwsQuestion, Long> {
 
 }
